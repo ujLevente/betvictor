f148fc17321ff6bb692fc1a60a485c8831948c77
ujLevente <asdlevente@gmail.com>
2023-03-14 01:13:35 +0100

    event controller unit tests added

--- src/event/event.controller.spec.ts
+++ src/event/event.controller.spec.ts
@@ -1,18 +1,90 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { Request } from 'express';
 import { EventController } from './event.controller';
+import { EventService } from './event.service';
 
 describe('EventController', () => {
-  let controller: EventController;
+    let controller: EventController;
+    let eventService: EventService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [EventController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [EventController],
+            providers: [EventService],
+        }).compile();
 
-    controller = module.get<EventController>(EventController);
-  });
+        controller = module.get<EventController>(EventController);
+        eventService = module.get<EventService>(EventService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('findAll', () => {
+        it('should call eventService.findAll with request.url', async () => {
+            const request: Partial<Request> = {
+                url: '/sports/events',
+            };
+            const expected = ['event1', 'event2'];
+
+            jest.spyOn(eventService, 'findAll').mockResolvedValue(expected);
+
+            const result = await controller.findAll(request as any);
+
+            expect(eventService.findAll).toHaveBeenCalledWith(request.url);
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('findAllBySportId', () => {
+        it('should call eventService.findEventsBySportId with sportId and request.url', async () => {
+            const request: Partial<Request> = {
+                url: '/sports/1/events',
+            };
+            const sportId = 1;
+            const expected = ['event1', 'event2'];
+
+            jest.spyOn(eventService, 'findEventsBySportId').mockResolvedValue(
+                expected,
+            );
+
+            const result = await controller.findAllBySportId(
+                sportId,
+                request as any,
+            );
+
+            expect(eventService.findEventsBySportId).toHaveBeenCalledWith(
+                sportId,
+                request.url,
+            );
+            expect(result).toEqual(expected);
+        });
+    });
+
+    describe('findOne', () => {
+        it('should call eventService.findEvent with sportId, eventId, and request.url', async () => {
+            const request: Partial<Request> = {
+                url: '/sports/1/events/1',
+            };
+            const sportId = 1;
+            const eventId = 1;
+            const expected = 'event1';
+
+            jest.spyOn(eventService, 'findEvent').mockResolvedValue(expected);
+
+            const result = await controller.findOne(
+                sportId,
+                eventId,
+                request as any,
+            );
+
+            expect(eventService.findEvent).toHaveBeenCalledWith(
+                sportId,
+                eventId,
+                request.url,
+            );
+            expect(result).toEqual(expected);
+        });
+    });
 });
